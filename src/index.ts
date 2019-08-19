cda8f3b543091536036a287d985fe7e774556e4e
PH <paulo@casion.com.br>
2019-08-19 16:41:59 -0300

    gzip configurado

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-const compress = (config: ICompressConfig) => {
+import middy from 'middy';
+import {gzip} from 'node-gzip';
+
+const compress: middy.Middleware<ICompressConfig> = (config: ICompressConfig | undefined) => {
   return {
-    after: (handler: any, next: any) => {
-      next();
+    after: (handler:middy.HandlerLambda<any,any>, next: middy.NextFunction) => {
+      gzip(handler.response.body)
+        .then(response=>{
+          handler.response.body = response.toString('base64');
+          handler.response.isBase64Encoded = true;
+          next();
+        })
+        .catch(err=>next(err));
     },
   };
 };
