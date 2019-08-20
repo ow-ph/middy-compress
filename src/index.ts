e7448a7108a987cd30aa7fe6f52bbfc0f71ffffd
PH <paulo@casion.com.br>
2019-08-20 15:03:04 -0300

    add logger

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import middy from 'middy';
 import {gzip} from 'node-gzip';
 
 const compress: middy.Middleware<ICompressConfig> = (config: ICompressConfig | undefined) => {
+  const defaultLogger = (data:any) => { };
+  const logger = (config && config.logger) || defaultLogger;
   return {
     after: (handler:middy.HandlerLambda<any,any>, next: middy.NextFunction) => {
       if (!handler.response.body){
+        logger('middy-compress::after - no body to gzip');
         next();
         return;
       }
@@ -14,35 +17,27 @@ const compress: middy.Middleware<ICompressConfig> = (config: ICompressConfig | u
           handler.response.isBase64Encoded = true;
           handler.response.headers = handler.response.headers || {};
           handler.response.headers["Content-Encoding"] = "gzip";
+
+          logger('middy-compress::after - content g zipped');
+
           next();
         })
-        .catch(err=>next(err));
+        .catch(err=> {
+          logger('middy-compress::after - error');
+          logger(err);
+          next(err)
+        });
     },
 
     before: (handler:middy.HandlerLambda<any,any>, next: middy.NextFunction) => {
       if (handler.event.isBase64Encoded && handler.event.body) {
+        logger('middy-compress::before - decoding base64');
+
         const buff = new Buffer(handler.event.body, 'base64');
         handler.event.body = buff.toString('ascii');
       }
 
       next();
-    },
-
-    onError: (handler:middy.HandlerLambda<any,any>, next: middy.NextFunction) => {
-      if (!handler.response.body){
-        next(handler.error);
-        return;
-      }
-      
-      gzip(handler.response.body)
-        .then(response=>{
-          handler.response.body = response.toString('base64');
-          handler.response.isBase64Encoded = true;
-          handler.response.headers = handler.response.headers || {};
-          handler.response.headers["Content-Encoding"] = "gzip";
-          next(handler.error);
-        })
-        .catch(err=>next(handler.error));
     },
 
   };
@@ -53,4 +48,5 @@ export { compress };
 export interface ICompressConfig {
   ignoreAcceptEncodingHeader?: boolean;
   verbose?: boolean;
+  logger?: (args:any)=>void
 }
